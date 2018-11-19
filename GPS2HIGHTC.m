e7c6d75396e5aef5834d2bd5d9c78f627f27461f
ken-hsu <kenhhsu@gmail.com>
2018-11-19 16:18:31 +0000

    add
    add all

--- GPS2HIGHTC.m
+++ GPS2HIGHTC.m
@@ -2,7 +2,7 @@
 % A function with a given DEM, input a GPS location, output a terrian
 % Input double(Longitude,Altitude)
 % Output int16(Hight)
-function H = GPS2HIGHTC(Latitude,Lontitude,map) %lat:10 lon:9
+function [H,c_record] = GPS2HIGHTC(Latitude,Lontitude,map) %lat:10 lon:9
     MapSize = 3601;
     amount = size(Latitude,1);
     for i = 1:amount
@@ -39,6 +39,7 @@ function H = GPS2HIGHTC(Latitude,Lontitude,map) %lat:10 lon:9
     %     end
     %     H = map(c(1),c(2));
     %     H = (map(c(1),c(2))+map(c1(1),c1(2))+map(c2(1),c2(2)))/3;
-          H(i) = max([map(c(1),c(2)), map(c1(1),c1(2)) ,map(c2(1),c2(2)) ]);
+    c_record(i,:) = c;
+          H(i,1) = max([map(c(1),c(2)), map(c1(1),c1(2)) ,map(c2(1),c2(2)) ]);
     end
 end
--- vertical_waypoint.m
+++ vertical_waypoint.m
@@ -1,9 +1,9 @@
 clear all;
-map = imread('ASTGTM2_N22E120_dem.tif');
-waypoint = [22.1202,120.1023;
-            22.9020,120.4012;
-            22.7020,120.9014;
-            22.9020,120.7021];
+map = imread('ASTGTM2_N24E121_dem.tif');
+waypoint = [22.9202,120.9023;
+            22.7020,120.7012;
+            22.4020,120.4014;
+            22.1020,120.1021];
         
 %% Increase waypoint
 step_accu = 0;
@@ -21,17 +21,39 @@ waypoint_add(1+step_accu,:) = waypoint(end,:);
 
 %% Calculate distance
 InitLat = waypoint(1,1);
+dis_accu(1,1) = 0;
 MetperLat = 110574;
 MetperLon = 111320*cos(InitLat*pi/180); % meter per degree
 for i = 1:size(waypoint_add,1)-1
     dis_latlon = waypoint_add(i+1,:) - waypoint_add(i,:);
     dis_meter(i,1) = sqrt((dis_latlon(1)*MetperLat)^2  + (dis_latlon(2)*MetperLon)^2);
+    dis_accu(i+1,1) = dis_accu(i,1) + dis_meter(i,1);
 end
 
 %% Calculate slope
-H = GPS2HIGHTC(waypoint_add(:,1),waypoint_add(:,2),map);
-for i = 1 :size(H,2)-1
-    H(i+1) - H(i)
-    slope = double(H)/dis_meter;
+[H,c_record] = GPS2HIGHTC(waypoint_add(:,1),waypoint_add(:,2),map);
+for i = 1 :size(H,1)-1
+    hight_diff = H(i+1) - H(i);
+    slope(i,1) = double(hight_diff)/dis_meter(i);
 end
-
+% ss = 1;
+% ee = 3601;
+% [xx,yy] = meshgrid(ss:ee,ss:ee);
+% mesh(xx,yy,map(ss:ee,ss:ee));
+% hold on;
+% plot3(c_record(:,2),c_record(:,1),H,'*r');
+%% slope judgement
+hight = double(H);
+slope_threshold = 0.9;
+for i = 1:size(slope,1)
+    if slope(i,1) > slope_threshold
+        num_of_advance = 0;
+        slope_adjust = slope_threshold+1;
+        while (~isempty(find(slope_adjust>slope_threshold)))
+            num_of_advance = num_of_advance + 1;
+            hight_change = linspace(hight(i-num_of_advance),hight(i+1),num_of_advance+2);
+            hight(i-num_of_advance:i+1) = hight_change;
+            slope_adjust = 
+        end
+    end
+end
